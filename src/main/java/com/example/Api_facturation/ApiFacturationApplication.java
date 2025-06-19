55ee543bf2ef8fe455c039489804e345d2e63c8b
BryanFongang <bryanofongang@gmail.com>
2025-06-19 11:57:34 +0100

    first commit

---
+++ src/main/java/com/example/Api_facturation/ApiFacturationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Api_facturation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFacturationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFacturationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Api_facturation/ApiFacturationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Api_facturation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFacturationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
